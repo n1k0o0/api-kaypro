3106d219cd6976ac5451e8f3019b2871bd26f13f
Nicat Ismayilov <n1ko.net.dev@gmail.com>
2021-11-24 12:28:50 +0400

    ForceJson (fix) change return type

--- app/Http/Middleware/ForceJson.php
+++ app/Http/Middleware/ForceJson.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ForceJson
@@ -12,9 +13,9 @@ class ForceJson
      *
      * @param  Request  $request
      * @param  Closure  $next
-     * @return mixed
+     * @return JsonResponse
      */
-    public function handle(Request $request, Closure $next): mixed
+    public function handle(Request $request, Closure $next): JsonResponse
     {
         $request->headers->set('Accept', 'application/json');
         return $next($request);
